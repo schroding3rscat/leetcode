90ac46d57b2ad164d4b90576b4b1180c50a29350
Akhmed Kharaev <haraev.ahmed@wildberries.ru>
2020-10-25 15:59:14 +0300

    Add solution for 28 max tree depth

---
+++ 28-max-depth/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	root := &TreeNode{
+		Val:  3,
+		Left: &TreeNode{Val: 9},
+		Right: &TreeNode{
+			Val:   20,
+			Left:  &TreeNode{Val: 15},
+			Right: &TreeNode{Val: 7},
+		},
+	}
+	fmt.Println(maxDepth(root))
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// TreeNode ...
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
